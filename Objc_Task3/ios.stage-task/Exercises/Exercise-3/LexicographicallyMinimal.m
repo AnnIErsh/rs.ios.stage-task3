393dcc6ab529d08d76b7435fb54124ab16c4cf0d
Anna <annaersh1994@gmail.com>
2021-05-22 01:29:39 +0300

    feat: add ex3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,33 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    NSMutableString *str = [NSMutableString string];
+    const char *s1 = [string1 UTF8String];
+    const char *s2 = [string2 UTF8String];
+    size_t len1 = string1.length;
+    size_t len2 = string2.length;
+    int i = 0;
+    int j = 0;
+    for ( ;i < string1.length && j < string2.length; )
+    {
+       if (*(s1 + i) <= *(s2 + j))
+       {
+           [str appendString:[NSString stringWithFormat:@"%c", *(s1 + i)]];
+           i++;
+           len1--;
+       }
+       else
+       {
+           [str appendString:[NSString stringWithFormat:@"%c", *(s2 + j)]];
+           j++;
+           len2--;
+       }
+    }
+    if (len1)
+        [str appendString:[string1 substringFromIndex:i]];
+    if (len2)
+        [str appendString:[string2 substringFromIndex:j]];
+    return str;
 }
 
 @end
